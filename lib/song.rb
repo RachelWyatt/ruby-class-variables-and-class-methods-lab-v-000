c67d3c8c77a7381819015b3eec0ea22a674afa68
Rachel Wyatt <rw126@evansville.edu>
2018-09-17 11:31:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,16 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@artist = []
+  @@genre = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artist << artist
+    @@genre << genre
   end
 
   def self.count
